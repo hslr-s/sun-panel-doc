8366b954e2facc83375447749cc50827dbda60b9
sun <95302870@qq.com>
2025-02-17 13:12:47 +0800

    增加广告组件 Squashed commit of the following:
    commit 83414243ba24ea706c708c2557c3d02d4bd60ea3
    Author: sun <95302870@qq.com>
    Date:   Mon Feb 17 13:11:21 2025 +0800
    
        配置广告信息
    
    commit 721a601c6670c9c94ba2154e29ae761aabe74999
    Author: sun <95302870@qq.com>
    Date:   Mon Feb 17 13:11:02 2025 +0800
    
        完成广告组件开发
    
    commit 6d2fab0c3216b96a75f72c3b3a769284365e4efb
    Author: sun <95302870@qq.com>
    Date:   Fri Feb 14 22:27:46 2025 +0800
    
        优化点击跳转链接追加用户id参数
    
    commit 4a6b37483fb037f45b43fde4adeee453461bc6f3
    Author: sun <95302870@qq.com>
    Date:   Fri Feb 14 14:21:37 2025 +0800
    
        支持保存用户的id
    
    commit 6a3613a68b223f06f3680a51ddd9b740e79eb844
    Author: sun <95302870@qq.com>
    Date:   Fri Feb 14 12:46:34 2025 +0800
    
        增加api域名可配置功能
    
    commit a5d6946d89a91e05101fd16387fe8e8e572c77fd
    Author: sun <95302870@qq.com>
    Date:   Thu Feb 13 22:31:17 2025 +0800
    
        增加广告组件以及自己开发的组件

---
+++ docs/.vitepress/theme/components/sun-ad/index.ts
@@ -0,0 +1,148 @@
+import { h, render } from 'vue'
+
+const getPositionInfoAplUrl = (domain:string,positionId: string) => {
+  return `${domain}/api/adPosition/getInfo/${positionId}`
+}
+
+interface AdItem {
+  description: string;
+  clickUrl: string;
+  isExpired: boolean;
+  imgUrl: string;
+  name: string;
+}
+
+interface AdvertisingPositionOptions {
+  className: string;
+  positionId: string;
+  apiDomain?: string;
+  first?: boolean;
+  width?: string;
+  height?: string;
+}
+
+interface Storage {
+  uid:string
+}
+
+const className = "SunAdvertisingPosition"
+const storageName = className
+
+const defaultOptions: AdvertisingPositionOptions = {
+  className,
+  positionId: "",
+  apiDomain: "https://goad.enianteam.com",
+  first: true,
+  width: "100%",
+  height: "100%",
+}
+
+function getStorage(): Storage {
+  const storage = localStorage.getItem(storageName);
+  if (storage) {
+    return JSON.parse(storage);
+  }
+  return {
+    uid: ""
+  }
+}
+
+function setStorage(storage: Storage){
+  localStorage.setItem(storageName, JSON.stringify(storage))
+}
+
+async function getPositionInfo(apiDomain:string,positionId: string,uid: string|null) {
+
+  return await fetch(getPositionInfoAplUrl(apiDomain,positionId), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    // body: uid!==""?JSON.stringify({ uid }):""
+  })
+    .then((response) => response.json())
+}
+
+function appendQueryParam(url: string, key: string, value: string): string {
+  // 解析 URL
+  const urlObj = new URL(url);
+
+  // 追加或更新参数
+  urlObj.searchParams.set(key, value);
+
+  // 返回完整的 URL 字符串
+  return urlObj.toString();
+}
+
+export function deleteByClassName(className: string) {
+  const existElement = document.querySelectorAll("." + className)
+  if (existElement) {
+    existElement.forEach(item => {
+      item.remove()
+    })
+  }
+}
+
+export default async function sunAdvertisingPosition(options: AdvertisingPositionOptions) {
+
+  let adList: AdItem[] = []
+
+  const optionsCfg= {
+    ...defaultOptions,
+    ...options
+  }
+
+  const s = getStorage();
+
+  await getPositionInfo(optionsCfg.apiDomain || "",optionsCfg.positionId,s.uid).then((data) => {
+    if (data.code === 0) {
+      adList = data.data.list as AdItem[]; 
+      // 保存用户id
+      if(s.uid===""){
+        s.uid = data.data.uid;
+        setStorage(s);
+      }
+    } else {
+      // console.error("Failed to fetch position info:", data.msg);
+    }
+  })
+    .catch((error) => {
+      // console.error("Error fetching position info:", error);
+    });
+
+  const name = className + "-" + optionsCfg.positionId
+  // 创建前先删除已存在元素
+  deleteByClassName(name)
+  const vnode = h("div", {
+    class: name
+  }, adList.map(ad => h(
+    "img",
+    {
+      src: ad.imgUrl,
+      alt: ad.description,
+      title: ad.description,
+      onclick: () => {
+        window.open(ad.clickUrl);
+      },
+      style: {
+        width: optionsCfg.width || "100%",
+        height: optionsCfg.height || "100%",
+        cursor: "pointer",
+        margin: "2px auto"
+      }
+    },
+  )));
+
+  const targetElement = document.querySelector(optionsCfg.className);
+  if (targetElement) {
+    // 是否插入到第一个元素
+    if (options.first) {
+      const container = document.createElement("div");
+      render(vnode, container);
+      targetElement.insertBefore(container.firstChild as HTMLDivElement, targetElement.firstChild);
+    } else {
+      render(vnode, targetElement);
+    }
+  }
+}
